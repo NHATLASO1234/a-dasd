f45c4311db2b36a8d57583725efe0ee7826cbf22
NHATLASO1234 <81008060+NHATLASO1234@users.noreply.github.com>
2021-07-13 09:41:03 +0700

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string myString = Console.ReadLine();
+            int charPos = myString.IndexOf("a");
+            string name = myString.Substring(charPos);
+            Console.WriteLine(name);
+        }
+    }
+}
+
