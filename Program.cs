f23bd8f00646339e49a2716cc7338fc83fee97a5
NHATLASO1234 <81008060+NHATLASO1234@users.noreply.github.com>
2021-07-13 09:21:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("It's a bad one ");
+            }
+            else
+            {
+                Console.WriteLine("It's a good one");
+            }
+        }
+    }
+}
